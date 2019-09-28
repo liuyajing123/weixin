d94bda09b86ceeec86a06acb0c220108ab0087d5
liuyajing123 <2972762365@qq.com>
2019-09-28 10:25:50 +0800

    aa

--- app/Http/Controllers/aaa/eventController.php
+++ app/Http/Controllers/aaa/eventController.php
@@ -36,7 +36,7 @@ class eventController extends Controller
             if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
                 //关注
                 //opnid拿到用户基本信息
-                $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->tools->get_wechat_access_token().'&openid='.$xml_arr['FromUserName'].'&lang=zh_CN';
+                $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->tools->get_access_token().'&openid='.$xml_arr['FromUserName'].'&lang=zh_CN';
                 $user_re = file_get_contents($url);
                 $user_info = json_decode($user_re,1);
                 //存入数据库
